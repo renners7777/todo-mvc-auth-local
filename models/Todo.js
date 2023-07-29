9569461ce161b83bf50ad97b49b8db13259be58a
Chris <104800006+renners7777@users.noreply.github.com>
2023-07-29 15:58:27 +0100

    Changed Model Todo file

--- models/Todo.js
+++ models/Todo.js
@@ -16,3 +16,5 @@ const TodoSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('Todo', TodoSchema)
+
+//Mongoose helps us create all of our documents. Documents are objects. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection. Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document. 
